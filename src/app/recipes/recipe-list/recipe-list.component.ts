d9ec87403b28b999732cec29274f9c736b77cc9b
MELINDA TÓTH <toth.melinda1982@gmail.com>
2021-12-30 17:23:35 +0100

    giving shopping-list and recipe lists

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe',
+      'This is a simple test recipe',
+      'https://streetkitchen.hu/wp-content/uploads/2021/05/202104130709-767x915.jpg'),
+    new Recipe(
+      'A test recipe',
+      'This is a simple test recipe',
+      'https://streetkitchen.hu/wp-content/uploads/2021/05/202104130709-767x915.jpg'),
+    new Recipe(
+      'A test recipe',
+      'This is a simple test recipe',
+      'https://streetkitchen.hu/wp-content/uploads/2021/05/202104130709-767x915.jpg')
+  ];
 
   constructor() { }
 
